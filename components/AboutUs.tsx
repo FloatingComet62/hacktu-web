f6938de80044c998e8b0544dbcf347494f86dbff
Aargh Rai <shubh2007rai@gmail.com>
2026-01-12 18:53:56 +0530

    card around about

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -4,6 +4,7 @@ import { HACKATHON_VERSION } from "@/app/config";
 import { useWidthCutoff } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { Card } from "./ui/card";
 
 export default function AboutUs() {
   const hideImage = useWidthCutoff(1000);
@@ -12,7 +13,7 @@ export default function AboutUs() {
     <div id="aboutus" className="flex flex-col items-center">
       <div className={cn("font-[Nippo-Variable] mb-16", verySmall ? "text-6xl" : "text-8xl")}>About us</div>
       <div className="flex w-[90vw] justify-evenly items-center">
-        <div className={cn("flex flex-col gap-4 text-3xl", hideImage ? "text-md" : "ml-8")}>
+        <Card className={cn("flex flex-col gap-4 text-3xl p-4", hideImage ? "text-md" : "ml-8")}>
           <div><span className="text-[var(--maindark)] font-bold">Creative Computing Society</span> is back with <span className="text-[var(--maindark)] font-bold">HackTU {HACKATHON_VERSION}.0</span>, our flagship event
           hosted at Thapar Institute of Engineering and Technology, Patiala.
           We're excited to see what innovative ideas emerge from this year's event.</div>
@@ -21,7 +22,7 @@ export default function AboutUs() {
           Today's tech world demands solutions that are both cutting-edge and user-friendly.</div>
           <div><span className="text-[var(--maindark)] font-bold">Gear up your tech stack</span>, brainstorm innovative ideas, ignite team spirit,
           and build solutions for real-world problems at <span className="text-[var(--maindark)] font-bold">HackTU {HACKATHON_VERSION}.0</span> - the biggest offline hackathon at TIET!</div>
-        </div>
+        </Card>
         {hideImage || (
           <Image src="/about_graphic.png" alt="About graphic" width={400} height={400} />
         )}
