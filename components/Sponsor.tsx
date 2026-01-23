af97c67e6f5cf24b4ac02e73ed4aca5bc09834ad
CodingWithSaksham <sakshamthecoder@gmail.com>
2026-01-23 22:59:32 +0530

    Added PPI offer tag to Dilaton's tracks
    Some files were also accidenlty edited by my linter so ignore them, rest works

--- components/Sponsor.tsx
+++ components/Sponsor.tsx
@@ -1,33 +1,71 @@
 "use client";
 
-import { NORMAL_SPONSOR, SOCIALS, SPONSOR_PACKET, SPONSORS } from "@/app/config"
-import Image from "next/image"
-import Link from "next/link"
-import { Button } from "./ui/button"
+import {
+  NORMAL_SPONSOR,
+  SOCIALS,
+  SPONSOR_PACKET,
+  SPONSORS,
+} from "@/app/config";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
 import { useWidthCutoff } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
 import { Card } from "./ui/card";
 
-const SHADOWCOLOR = ["red", "yellow", "green", "blue", "purple"]
+const SHADOWCOLOR = ["red", "yellow", "green", "blue", "purple"];
 export default function Sponsor() {
   const verySmall = useWidthCutoff(484);
   const twoColumns = useWidthCutoff(632);
   const aBitSmall = useWidthCutoff(1000);
   return (
     <div id="sponsor" className="flex flex-col items-center">
-      <div className={cn("font-[Nippo-Variable] mb-16", verySmall ? "text-6xl" : "text-8xl")}>Sponsor</div>
-      <div className={cn("flex flex-col w-[100vw] gap-15 justify-evenly items-center text-center", verySmall ? "p-2" : "p-8")}>
+      <div
+        className={cn(
+          "font-[Nippo-Variable] mb-16",
+          verySmall ? "text-6xl" : "text-8xl",
+        )}
+      >
+        Sponsor
+      </div>
+      <div
+        className={cn(
+          "flex flex-col w-[100vw] gap-15 justify-evenly items-center text-center",
+          verySmall ? "p-2" : "p-8",
+        )}
+      >
         <div className="flex flex-col gap-4">
           {Object.entries(SPONSORS).map(([title, sponsors], i) => (
-            <Card className={cn("flex flex-col gap-4 p-4 justify-center", aBitSmall ? "w-[90vw]" : "w-[50vw]")}>
-              {(title != NORMAL_SPONSOR) && (
+            <Card
+              className={cn(
+                "flex flex-col gap-4 p-4 justify-center",
+                aBitSmall ? "w-[90vw]" : "w-[50vw]",
+              )}
+            >
+              {title != NORMAL_SPONSOR && (
                 <div className="text-4xl font-[Nippo-Variable]">{title}</div>
               )}
-              <div className={cn("grid gap-4", twoColumns ? "grid-cols-1" : "grid-cols-3")}>
+              <div
+                className={cn(
+                  "grid gap-4",
+                  twoColumns ? "grid-cols-1" : "grid-cols-3",
+                )}
+              >
                 {sponsors.map(({ image_link, link, name }, j) => (
-                  <Card key={`sponsor-${name}`} className={cn("flex items-center justify-center p-8", SHADOWCOLOR[(i + j) % SHADOWCOLOR.length] + "shadow")}>
+                  <Card
+                    key={`sponsor-${name}`}
+                    className={cn(
+                      "flex items-center justify-center p-8",
+                      SHADOWCOLOR[(i + j) % SHADOWCOLOR.length] + "shadow",
+                    )}
+                  >
                     <Link href={link}>
-                      <Image src={image_link} width={600} height={400} alt={name} />
+                      <Image
+                        src={image_link}
+                        width={600}
+                        height={400}
+                        alt={name}
+                      />
                     </Link>
                   </Card>
                 ))}
@@ -35,16 +73,33 @@ export default function Sponsor() {
             </Card>
           ))}
         </div>
-        <div className={cn("flex flex-col gap-4 font-[Nippo-Variable]", verySmall ? "w-[90vw]" : "")}>
-          <div className={cn(verySmall ? "text-3xl" : "text-6xl")}>Looking to Sponsor?</div>
-          <div className={cn(verySmall ? "text-2xl" : "text-4xl")}>See our sponsor packet below and reach out to us</div>
-          <Link href={SOCIALS.mail} className="text-2xl underline">here!</Link>
+        <div
+          className={cn(
+            "flex flex-col gap-4 font-[Nippo-Variable]",
+            verySmall ? "w-[90vw]" : "",
+          )}
+        >
+          <div className={cn(verySmall ? "text-3xl" : "text-6xl")}>
+            Looking to Sponsor?
+          </div>
+          <div className={cn(verySmall ? "text-2xl" : "text-4xl")}>
+            See our sponsor packet below and reach out to us
+          </div>
+          <Link href={SOCIALS.mail} className="text-2xl underline">
+            here!
+          </Link>
           <Link href={SPONSOR_PACKET}>
-            <Button className={cn("h-auto p-4 cursor-pointer w-full", verySmall ? "text-3xl" : "text-5xl")}>Sponsor Packet</Button>
+            <Button
+              className={cn(
+                "h-auto p-4 cursor-pointer w-full",
+                verySmall ? "text-3xl" : "text-5xl",
+              )}
+            >
+              Sponsor Packet
+            </Button>
           </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
-
--- components/Tracks.tsx
+++ components/Tracks.tsx
@@ -1,71 +1,171 @@
 "use client";
 
-import { DILATHON_SPONSORED_TRACKS, TRACKS } from "@/app/config"
+import { DILATHON_SPONSORED_TRACKS, TRACKS } from "@/app/config";
 import { Card, CardTitle } from "./ui/card";
 import { cn } from "@/lib/utils";
 import { useWidthCutoff } from "@/hooks/use-mobile";
-import Link from "next/link";
 
-const SHADOWCOLOR = ["red", "yellow", "green", "blue", "purple"]
+const SHADOWCOLOR = ["red", "yellow", "green", "blue", "purple"];
+
 export default function Tracks() {
   const smallerFont = useWidthCutoff(582);
   const switchToSingleColumn = useWidthCutoff(1144);
   const verySmall = useWidthCutoff(484);
+
   return (
     <div id="tracks" className="flex flex-col items-center">
-      <div className={cn("font-[Nippo-Variable] mb-16", verySmall ? "text-6xl" : "text-8xl")}>Tracks</div>
-      <div className={cn("grid gap-4 w-[80vw] justify-evenly text-center", switchToSingleColumn ? "grid-cols-1" : "grid-cols-2")}>
+      <div
+        className={cn(
+          "font-[Nippo-Variable] mb-16",
+          verySmall ? "text-6xl" : "text-8xl",
+        )}
+      >
+        Tracks
+      </div>
+      <div
+        className={cn(
+          "grid gap-4 w-[80vw] justify-evenly text-center",
+          switchToSingleColumn ? "grid-cols-1" : "grid-cols-2",
+        )}
+      >
         {DILATHON_SPONSORED_TRACKS.map((track, i) => (
-          <Card key={`dilathon-track-${i}`} className={cn("p-4 flex flex-col h-auto justify-evenly", SHADOWCOLOR[i % SHADOWCOLOR.length] + "shadow")}>
+          <Card
+            key={`dilathon-track-${i}`}
+            className={cn(
+              "p-4 flex flex-col h-auto justify-evenly",
+              SHADOWCOLOR[i % SHADOWCOLOR.length] + "shadow",
+            )}
+          >
             <CardTitle>
-              <div className={cn("font-[Nippo-Variable]", smallerFont ? "text-xl" : "text-4xl")}>{track.name}</div>
+              <div
+                className={cn(
+                  "font-[Nippo-Variable]",
+                  smallerFont ? "text-xl" : "text-4xl",
+                )}
+              >
+                {track.name}
+              </div>
             </CardTitle>
             <div className="flex flex-col gap-2 text-start">
               <div className="font-bold text-lg">Problem</div>
               <div className="text-lg text-[#fb542b]">{track.problem}</div>
               <div className="font-bold text-lg">Challenge</div>
               <div className="text-lg text-[#68b8ff]">{track.challenge}</div>
-              <div className="text-lg text-[#76b963]">Get an opportunity to secure Pre Placement Interview (PPI) with Dilathon</div>
-              {track.attachments.length > 0 && (
-                <>
-                  <div className="font-bold text-lg">Attachments</div>
-                  <div className="flex flex-col ml-4">
-                    {track.attachments.map(({ link, label }, j) => (
-                      <Link key={`dilathon-track-${i}-requirement-${j}`} href={link}>{label}</Link>
-                    ))}
+              <div className="font-bold text-lg">Requirements</div>
+              <div className="flex flex-col ml-4">
+                {track.requirements.map((requirement, j) => (
+                  <div key={`dilathon-track-${i}-requirement-${j}`}>
+                    {requirement}
                   </div>
-                </>
-              )}
-              {track.requirements.length > 0 && (
-                <>
-                  <div className="font-bold text-lg">Requirements</div>
-                  <div className="flex flex-col ml-4">
-                    {track.requirements.map((requirement, j) => (
-                      <div key={`dilathon-track-${i}-requirement-${j}`}>{requirement}</div>
-                    ))}
-                  </div>
-                </>
-              )}
+                ))}
+              </div>
+
+              <div
+                className={cn(
+                  "mt-4 p-3 border-l-4",
+                  SHADOWCOLOR[i % SHADOWCOLOR.length] + "border",
+                )}
+              >
+                <div
+                  className={cn(
+                    "font-extrabold text-xl uppercase",
+                    SHADOWCOLOR[i % SHADOWCOLOR.length] + "text",
+                  )}
+                >
+                  PPI Opportunity
+                </div>
+
+                <div className="font-bold text-lg text-[#1C1C1C] mt-1">
+                  Selecting this track makes you eligible for a
+                  <span
+                    className={cn(
+                      "ml-1",
+                      SHADOWCOLOR[i % SHADOWCOLOR.length] + "text",
+                    )}
+                  >
+                    Pre-Placement Offer (PPI)
+                  </span>{" "}
+                  from Dilaton.
+                </div>
+              </div>
             </div>
           </Card>
         ))}
-        <Card className={cn("p-4 flex flex-col h-auto justify-evenly", SHADOWCOLOR[(DILATHON_SPONSORED_TRACKS.length) % SHADOWCOLOR.length] + "shadow")}>
+        <Card
+          className={cn(
+            "p-4 flex flex-col h-auto justify-evenly",
+            SHADOWCOLOR[DILATHON_SPONSORED_TRACKS.length % SHADOWCOLOR.length] +
+              "shadow",
+          )}
+        >
           <CardTitle>
-            <div className={cn("font-[Nippo-Variable]", smallerFont ? "text-xl" : "text-4xl")}>OPEN DEFENCE INNOVATION</div>
+            <div
+              className={cn(
+                "font-[Nippo-Variable]",
+                smallerFont ? "text-xl" : "text-4xl",
+              )}
+            >
+              OPEN DEFENCE INNOVATION
+            </div>
           </CardTitle>
           <div className="text-md">
-Surprise us! Propose + prototype any defense-relevant drone solution:
-Counter-UAV jamming
-Swarm coordination
-Thermal target tracking
-Autonomous package delivery (ISR)
-Your wildest idea...
+            Surprise us! Propose + prototype any defense-relevant drone
+            solution: Counter-UAV jamming Swarm coordination Thermal target
+            tracking Autonomous package delivery (ISR) Your wildest idea...
+          </div>
+
+          <div
+            className={cn(
+              "mt-4 p-3 border-l-4 text-left",
+              SHADOWCOLOR[
+                DILATHON_SPONSORED_TRACKS.length % SHADOWCOLOR.length
+              ] + "border",
+            )}
+          >
+            <div
+              className={cn(
+                "font-extrabold text-xl uppercase",
+                SHADOWCOLOR[
+                  DILATHON_SPONSORED_TRACKS.length % SHADOWCOLOR.length
+                ] + "text",
+              )}
+            >
+              PPI Opportunity
+            </div>
+
+            <div className="font-bold text-lg text-[#1C1C1C] mt-1">
+              Selecting this track makes you eligible for a
+              <span
+                className={cn(
+                  "ml-1",
+                  SHADOWCOLOR[
+                    DILATHON_SPONSORED_TRACKS.length % SHADOWCOLOR.length
+                  ] + "text",
+                )}
+              >
+                Pre-Placement Offer (PPI)
+              </span>{" "}
+              from Dilaton.
+            </div>
           </div>
         </Card>
         {TRACKS.map((track, i) => (
-          <Card key={`track-${i}`} className={cn("p-4 flex flex-col h-auto justify-evenly", SHADOWCOLOR[i % SHADOWCOLOR.length] + "shadow")}>
+          <Card
+            key={`track-${i}`}
+            className={cn(
+              "p-4 flex flex-col h-auto justify-evenly",
+              SHADOWCOLOR[i % SHADOWCOLOR.length] + "shadow",
+            )}
+          >
             <CardTitle>
-              <div className={cn("font-[Nippo-Variable]", smallerFont ? "text-2xl" : "text-6xl")}>{track.name}</div>
+              <div
+                className={cn(
+                  "font-[Nippo-Variable]",
+                  smallerFont ? "text-2xl" : "text-6xl",
+                )}
+              >
+                {track.name}
+              </div>
               <div className="text-lg">{track.sdg}</div>
             </CardTitle>
             <div className="text-md">{track.description}</div>
@@ -73,6 +173,5 @@ Your wildest idea...
         ))}
       </div>
     </div>
-  )
+  );
 }
-
